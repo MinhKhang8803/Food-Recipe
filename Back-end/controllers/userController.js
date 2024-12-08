5753a0c1046651067eeba529d6c6daacb7f18c3f
Minh Khang <khangbmgch210522@fpt.edu.vn>
2024-12-08 16:42:57 +0700

    1 line Error

--- Back-end/controllers/userController.js
+++ Back-end/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');  
-const BanUser = require('../models/BanUser');
+const BanUser = require('../models/Ban');
 
 exports.updateAvatar = async (req, res) => {
     const { avatarUrl } = req.body;
